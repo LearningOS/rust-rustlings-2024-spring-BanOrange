ef3c6e1f7ef6b012c2ef86830013ec07e581f61f
alporis <595630751@qq.com>
2024-08-05 21:15:48 +0800

    first class

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -3,11 +3,11 @@
 // Execute `rustlings hint variables5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
     let number = "T-H-R-E-E"; // don't change this line
     println!("Spell a Number : {}", number);
-    number = 3; // don't rename this variable
+    let number = 3; // don't rename this variable
+    //类似于一个语法糖，重新let这个变量就会舍弃掉原来的数据
     println!("Number plus two is : {}", number + 2);
 }
